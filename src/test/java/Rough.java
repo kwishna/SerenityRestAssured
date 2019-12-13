e688f162b31c8d4cae3192660bd3bc8cb47321e7
disguise <kwishna@gmail.com>
2019-12-13 22:00:36 +0530

    Added Files

---
+++ src/test/java/Rough.java
@@ -0,0 +1,30 @@
+import org.apache.commons.io.FileDeleteStrategy;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Rough {
+	public static void findTextInThisWorkSpace(String workSpace, String textToFind) {
+
+		List<File> l = (List<File>) FileUtils.listFiles(new File(workSpace), new String[]{"java", "xml"}, true);
+		l.parallelStream().filter(a -> {
+			String allData = "";
+			try {
+				allData = Files.readString(a.toPath());
+			} catch (IOException e) {
+				System.err.println("Error Here : " + a.getAbsolutePath());
+			}
+			return allData.toLowerCase().contains(textToFind.toLowerCase());
+		}).forEach(x -> System.out.println(x.getAbsolutePath()));
+	}
+
+	public static void main(String[] args) {
+
+		findTextInThisWorkSpace("F:/Personal/Locker/APNS-053.mp4/old_mm - Copy/AltraIntegratedSuite/New", "wipro");
+	}
+}
